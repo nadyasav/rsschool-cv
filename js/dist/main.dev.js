6207a088e7747acf6c35bffe3b538597f465ee4f
NSavkova <47262112+NSavkova@users.noreply.github.com>
2021-10-01 02:17:06 +0300

    feat: completely finished layout cv

---
+++ js/dist/main.dev.js
@@ -0,0 +1,29 @@
+"use strict";
+
+/*--Menu START--*/
+//Mobile menu
+var menuBtn = document.querySelector('.menu-btn');
+var closeBtn = document.querySelector('.close-btn');
+var menu = document.querySelector('.header__box');
+
+if (menuBtn != null) {
+  menuBtn.addEventListener('click', menuOpen);
+}
+
+if (closeBtn != null) {
+  closeBtn.addEventListener('click', closeMenu);
+}
+
+function menuOpen() {
+  //ShowMenu
+  menu.classList.add('active');
+  burgerMenuBtn.classList.add('active');
+  document.getElementsByTagName("body")[0].style.overflow = "hidden";
+}
+
+function closeMenu() {
+  //HideMenu
+  menu.classList.remove('active');
+  burgerMenuBtn.classList.remove('active');
+  document.getElementsByTagName("body")[0].style.overflow = "auto";
+}
---
+++ js/main.js
@@ -0,0 +1,27 @@
+/*--Menu START--*/
+    //Mobile menu
+
+    let menuBtn = document.querySelector('.menu-btn');
+    let closeBtn = document.querySelector('.close-btn');
+    let menu = document.querySelector('.header__box');
+
+    if(menuBtn != null){
+        menuBtn.addEventListener('click', menuOpen);
+    }
+    if(closeBtn != null){
+        closeBtn.addEventListener('click', closeMenu);
+    }
+
+    function menuOpen(){
+        //ShowMenu
+        menu.classList.add('active');
+        burgerMenuBtn.classList.add('active');
+        document.getElementsByTagName("body")[0].style.overflow = "hidden";
+    }
+
+    function closeMenu(){
+        //HideMenu
+        menu.classList.remove('active');
+        burgerMenuBtn.classList.remove('active');
+        document.getElementsByTagName("body")[0].style.overflow = "auto";
+    }
